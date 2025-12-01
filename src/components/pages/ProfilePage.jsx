a997971598120b25bf4524a82c4b1ee7918687cc
sashaprykhodko <CaH4aZzz@gmail.com>
2025-12-01 23:14:41 +0200

    added sessionStore and its usage on profile page.

--- src/components/pages/ProfilePage.jsx
+++ src/components/pages/ProfilePage.jsx
@@ -1,6 +1,26 @@
+import { useState } from "react";
+
 function ProfilePage() {
+  const NOT_COOKED_STATUS = 'NOT_COOKED';
+
+  const [myRecipes, setMyRecipes] = useState(() => {
+    return JSON.parse(sessionStorage.getItem('favorites')) || [];
+  });
+
+  const notCooked = myRecipes.filter(recipe => recipe.status === NOT_COOKED_STATUS);
+  console.log('notCooked');
+  console.log(notCooked);
+
   return (
-    <div>This is profile page</div>
+    <div>
+      <div>This is profile page</div>
+      <h2>Not cooked ({notCooked.length})</h2>
+      <ul>
+        {notCooked.map(recipe => (
+          <li key={recipe.id}> {recipe.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
--- src/components/recipes/RecipeCard.jsx
+++ src/components/recipes/RecipeCard.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react";
 import { IMAGE_PLACEHOLDER } from "../../constants";
 
 function RecipeCard({ recipe }) {
+  const recipeId = recipe.id;
   const title = recipe.name || 'Untitled';
   const difficulty = recipe.difficulty || 'Unknown';
   const image = recipe.image || IMAGE_PLACEHOLDER;
   const cookingTime = (recipe.cookTimeMinutes && recipe.prepTimeMinutes)
     ? `${recipe.prepTimeMinutes + recipe.cookTimeMinutes} mins` : 'N/A';
 
-  const handleClick = () => {
-    console.log(`Added "${title}" to Recipe Book!`);
+  const handleAddRecipe = (recipe) => {
+    console.log(`Added ${recipeId} to Recipe Book!`);
+    const stored = sessionStorage.getItem('favorites');
+    const recipeBook = stored ? JSON.parse(stored) : [];
+    const newRecipe = { ...recipe, status: 'NOT_COOKED' };
+    const updated = [...recipeBook, newRecipe];
+
+    sessionStorage.setItem('favorites', JSON.stringify(updated));
   }
 
   return (
     <div className="recipe-card">
       <img src={image} alt="Recipe Image" />
-      <button onClick={handleClick}>Add to Recipe Book</button>
+      <button onClick={() => handleAddRecipe(recipe)}>
+        Add to Recipe Book
+      </button>
       <h3>{title}</h3>
       <p> Difficulty: {difficulty}</p>
       <p> Cooking Time: {cookingTime}</p>
